d80b86fb87347415e4226f8e6040477c0b42a605
Gaurav <grvdhingra1999@gmail.com>
2020-02-26 16:44:11 +0530

    View FileSystem

---
+++ Lecture-1/Lecture-2/Facts/fspoc.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+let path = require("path");
+
+function displayList(src) {
+    let ans = fs.lstatSync(src).isDirectory();
+    if (ans == false) {
+        console.log(src + "*");
+    } else {
+        // directory
+        console.log(src);
+        let childrens = fs.readdirSync(src);
+        // console.log(childrens);
+        for (let i = 0; i < childrens.length; i++) {
+            let cChPath = path.join(src, childrens[i]);
+            displayList(cChPath);
+
+        }
+    }
+}
+
+displayList("./src");
